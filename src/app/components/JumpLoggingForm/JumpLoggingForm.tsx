8138184af6fb89737dce21e42b218a0b7a981bac
ioana996 <stroineaioana@gmail.com>
2026-02-19 21:03:19 +0100

    first basic form

---
+++ src/app/components/JumpLoggingForm/JumpLoggingForm.tsx
@@ -0,0 +1,37 @@
+const JumpLogginForm = () => {
+  return (
+    <form>
+      <label htmlFor="dropzone">Dropzone</label>
+      <br></br>
+      <input
+        type="text"
+        id="dropzone"
+        name="dropzone"
+        required
+        className="border-4 border-indigo-500"
+      />
+      <br></br>
+      <label htmlFor="date">Date</label>
+      <br></br>
+      <input
+        type="date"
+        id="date"
+        name="date"
+        required
+        className="border-4 border-indigo-500"
+      />
+      <br></br>
+      <label htmlFor="notes">Notes</label>
+      <br></br>
+      <input
+        type="textarea"
+        id="notes"
+        name="notes"
+        required
+        className="border-4 border-indigo-500"
+      />
+    </form>
+  );
+};
+
+export default JumpLogginForm;
